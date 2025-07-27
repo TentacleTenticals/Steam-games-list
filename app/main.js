c390e435ba1b6e86b842d2e50bd7edf449104c44
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-07-27 16:49:49 +0300

    Update main.js

--- app/main.js
+++ app/main.js
@@ -8,6 +8,27 @@ function writer(data){
   console.log('Completed');
 };
 
-writer({
-  test: 'fest'
-});
+const getList = (o) => fetch(o.url+ new URLSearchParams(o.params), {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}).then(
+  r => {
+    if(r.ok) return r.json();
+  }
+);
+
+getList({
+  url: 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/?',
+  format: 'json'
+}).then(
+  res => {
+    console.log('Res', res);
+    if(res) writer(res);
+  }
+);
+
+// writer({
+//   test: 'fest'
+// });
