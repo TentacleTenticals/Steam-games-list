07a193322659c970bf488dfe2975416d40618262
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-07-27 15:58:09 +0300

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 
 function writer(data){
+  console.log('Starting...');
   fs.writeFileSync(`./test.json`, JSON.stringify(data), (error) => {
     if(error) throw error;
   });
+  console.log('Completed');
 };
 
 writer({
