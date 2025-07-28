e83e8e5cd53f8264d2f4900338e65e34895ed4c2
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-07-28 13:20:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+import {El} from './src/m.js';
+// import {db} from './db.json';
+
 console.log('Yo!');
+
+class Func {
+  get = {
+    params: (o) => {
+      const urlParams = new URLSearchParams(document.location.search);
+      const data = {};
+
+      for(let [key, val] of urlParams.entries()){
+        console.log(`${key}, ${val}`);
+        data[key] = val;
+      };
+
+      return data;
+    }
+  },
+  write = {
+    list: async () => {
+      const data = this.get.params();
+      if(!data) return;
+      if(!data.key) return;
+
+      const db = await fetch('http://TentacleTenticals.github.io/Steam-games-list/db.json');
+      if(!db) return;
+      console.log('DB', db);
+
+      // const item = db.find(data.key);
+    }
+  }
+}
+
+new Func().write.list()
